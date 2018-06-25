be71d4ea5310dd4af709b7cd19853e131b2be288
juwitaw <juwita.winadwiastuti@dattabot.io>
2018-06-25 20:51:10 +0700

    add comment on contract

--- contracts/HaraToken.sol
+++ contracts/HaraToken.sol
@@ -446,8 +446,11 @@ contract CappedToken is MintableToken {
 // File: contracts/TokenERC20.sol
 
 contract HaraToken is BurnableToken, CappedToken {
-    string public name = "HARA Token";
-    string public symbol = "HART";
+    string public constant name = "HARA Token";
+    string public constant symbol = "HART";
+    uint8 public constant decimals = 18;
+    
+    uint256 public constant INITIAL_SUPPLY = 12 * (10 ** uint256(decimals));
 
     uint public nonce = 0;
     mapping (uint => bool) public mintStatus;
@@ -455,20 +458,49 @@ contract HaraToken is BurnableToken, CappedToken {
     event BurnLog(uint indexed id, address indexed burner, uint256 value, bytes32 hashDetails);
     event MintLog(uint indexed id, address indexed requester, uint256 value, bool status);
 
+    /**
+    * @dev Constructor that gives msg.sender all of existing tokens.
+    */
+    constructor() public {
+        totalSupply_ = INITIAL_SUPPLY;
+        balances[msg.sender] = INITIAL_SUPPLY;
+        emit Transfer(address(0), msg.sender, INITIAL_SUPPLY);
+    }
+    
+    /**
+    * @dev Function to burn tokens
+    * @param value The amount of tokens to burn.
+    */
     function burnToken(uint value) public {
         burn(value);
         emit BurnLog(nonce, msg.sender, value, hashDetails(nonce, msg.sender, value));
         nonce = nonce++;
     }
 
-    function mintToken(uint id, address requester, uint256 value, bytes32 hash) public {
+    /**
+    * @dev Function to mint tokens
+    * @param id The unique burn ID.
+    * @param requester The address that will receive the minted tokens.
+    * @param value The amount of tokens to mint.
+    * @param hash Generated hash from burn function.
+    * @return A boolean that indicates if the operation was successful.
+    */
+    function mintToken(uint id, address requester, uint256 value, bytes32 hash) public returns(bool) {
         require(mintStatus[id]==false, "id already requested for mint");
         bytes32 hashInput = hashDetails(id, requester, value);
         require(hashInput == hash, "request item are not valid");
         bool status = mint(requester, value);
         emit MintLog(id, requester, value, status);
+        return status;
     }
 
+    /**
+    * @dev Function to hash burn and mint details.
+    * @param id The unique burn ID.
+    * @param requester The address that will receive the minted tokens.
+    * @param value The amount of tokens to mint.
+    * @return bytes32 from keccak256 hash of inputs.
+    */
     function hashDetails(uint id, address burner, uint value) internal pure returns (bytes32) {
         return keccak256(abi.encodePacked(id, burner, value));
     }   
